15c5910dbcfd99b5fb37ed689dfb77cbedfab11d
Olena <anyanyme@gmail.com>
2023-06-01 16:12:23 +0200

    add loading and error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,31 @@
 
 import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsLoading, selectError } from 'redux/contacts/contactsSelectors';
+import { fetchContacts } from 'redux/contacts/contactsOperations';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactsList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import css from './App.module.css';
 
 export const App = () => {
+
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div className={css.container}>
       <h1 className={css.sectionTitle}>Phonebook</h1>
-  <ContactForm />
+      <ContactForm />
       <h2 className={css.sectionTitle}>Contacts</h2>
-     <Filter />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <Filter />
       <ContactsList />
     </div>
   );
